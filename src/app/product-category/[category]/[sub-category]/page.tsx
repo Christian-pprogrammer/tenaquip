e9db9818e38a622a4792bf3ccf51c15f732fb66f
Christian-pprogrammer <mpanoc6@gmail.com>
2024-03-22 01:26:00 +0200

    product page

--- src/app/product-category/[category]/[sub-category]/page.tsx
+++ src/app/product-category/[category]/[sub-category]/page.tsx
@@ -1,9 +1,124 @@
 import Breadcrump from "@/components/Breadcrump/Breadcrump";
 import DropdownComponent from "@/components/DropdownComponent/DropdownComponent";
 import FilterDropdown from "@/components/FilterDropdown/FilterDropdown";
+import ProductComponent from "@/components/ProductComponent/ProductComponent";
 import React from "react";
 
 const SubCategory = () => {
+  const categoryProducts = [
+
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+    {
+      imageUrl: "https://www.tenaquip.com/images/medium/s/sae731_ad.webp?1631622771",
+      manufacturer: "Brady",
+      productName: "Water Pipe Marker, Snap-On, 7/8 H x 7/8 W, White on Green",
+      model: "SAE731",
+      manufacturerModelNo: "4155-B",
+      unitPrice: "10",
+      shipsIn: "3",
+      bonusNumber: 5,
+      bonus: 10,
+    },
+  ];
+
   return (
     <div className="mx-32">
       <Breadcrump
@@ -28,7 +143,7 @@ const SubCategory = () => {
       />
 
       <div className="">
-        <h2 className="font-semibold text-2xl text-Gray my-2">Register</h2>
+        <h2 className="font-semibold text-2xl text-Gray my-2">Pipe Marker</h2>
         <p className="my-2 text-Gray text-sm">
           Showing 1 - 20 of 3832 listing(s)
         </p>
@@ -36,94 +151,107 @@ const SubCategory = () => {
 
       <div className="bg-lightMain p-2">
         <div className="filters">
-          
-          <FilterDropdown title="Manufacturer" elements={[
-            {
-              itemName: "Black on Orange (1)"
-            },
-            {
-              itemName: "Black on White (1)"
-            },
-            {
-              itemName: "Black on Yellow (44)"
-            },
-            {
-              itemName: "White on Blue (5)"
-            }
-            
-          ]}>
+          <FilterDropdown
+            title="Manufacturer"
+            elements={[
+              {
+                itemName: "Black on Orange (1)",
+              },
+              {
+                itemName: "Black on White (1)",
+              },
+              {
+                itemName: "Black on Yellow (44)",
+              },
+              {
+                itemName: "White on Blue (5)",
+              },
+            ]}
+          ></FilterDropdown>
 
-          </FilterDropdown>
-
-          <FilterDropdown title="Manufacturer" elements={[
-            {
-              itemName: "Black on Orange (1)"
-            },
-            {
-              itemName: "Black on White (1)"
-            },
-            {
-              itemName: "Black on Yellow (44)"
-            },
-            {
-              itemName: "White on Blue (5)"
-            }
-            
-          ]}>
-
-          </FilterDropdown>
-          <FilterDropdown title="Manufacturer" elements={[
-            {
-              itemName: "Black on Orange (1)"
-            },
-            {
-              itemName: "Black on White (1)"
-            },
-            {
-              itemName: "Black on Yellow (44)"
-            },
-            {
-              itemName: "White on Blue (5)"
-            }
-            
-          ]}>
-
-          </FilterDropdown><FilterDropdown title="Manufacturer" elements={[
-            {
-              itemName: "Black on Orange (1)"
-            },
-            {
-              itemName: "Black on White (1)"
-            },
-            {
-              itemName: "Black on Yellow (44)"
-            },
-            {
-              itemName: "White on Blue (5)"
-            }
-            
-          ]}>
-
-          </FilterDropdown><FilterDropdown title="Manufacturer" elements={[
-            {
-              itemName: "Black on Orange (1)"
-            },
-            {
-              itemName: "Black on White (1)"
-            },
-            {
-              itemName: "Black on Yellow (44)"
-            },
-            {
-              itemName: "White on Blue (5)"
-            }
-            
-          ]}>
-
-          </FilterDropdown>
+          <FilterDropdown
+            title="Manufacturer"
+            elements={[
+              {
+                itemName: "Black on Orange (1)",
+              },
+              {
+                itemName: "Black on White (1)",
+              },
+              {
+                itemName: "Black on Yellow (44)",
+              },
+              {
+                itemName: "White on Blue (5)",
+              },
+            ]}
+          ></FilterDropdown>
+          <FilterDropdown
+            title="Manufacturer"
+            elements={[
+              {
+                itemName: "Black on Orange (1)",
+              },
+              {
+                itemName: "Black on White (1)",
+              },
+              {
+                itemName: "Black on Yellow (44)",
+              },
+              {
+                itemName: "White on Blue (5)",
+              },
+            ]}
+          ></FilterDropdown>
+          <FilterDropdown
+            title="Manufacturer"
+            elements={[
+              {
+                itemName: "Black on Orange (1)",
+              },
+              {
+                itemName: "Black on White (1)",
+              },
+              {
+                itemName: "Black on Yellow (44)",
+              },
+              {
+                itemName: "White on Blue (5)",
+              },
+            ]}
+          ></FilterDropdown>
+          <FilterDropdown
+            title="Manufacturer"
+            elements={[
+              {
+                itemName: "Black on Orange (1)",
+              },
+              {
+                itemName: "Black on White (1)",
+              },
+              {
+                itemName: "Black on Yellow (44)",
+              },
+              {
+                itemName: "White on Blue (5)",
+              },
+            ]}
+          ></FilterDropdown>
+          <br />
           <button className="custom-btn font-bold">View All Filters</button>
         </div>
         <div className="sorts"></div>
+      </div>
+
+      <div className="grid md:grid-cols-4">
+        {
+          categoryProducts.map((product, index)=>(
+            <ProductComponent  
+              key={index}
+              product={product}
+            />
+          ))
+        }
       </div>
     </div>
   );
